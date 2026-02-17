57fbb52ee7599251b9539ab6d6dac348d0d86e0c
paulichdom <paulic.domagoj@gmail.com>
2026-02-17 09:26:21 +0100

    solution: use styled components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles.css";
+import { StyledComponentsRegistry } from "./components/StyledComponentsRegistry";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,8 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header>Main header</header>
-        {children}
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
         <footer>Page rendered on {formattedDate}</footer>
       </body>
     </html>
